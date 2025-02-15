3d4af0ea4ffabba678de9b8e585577ebdcac1bf9
Shagun <pshagun229@gmail.com>
2025-02-15 13:19:12 +0530

    Add files via upload

---
+++ task4_backup_recovery.sql
@@ -0,0 +1,9 @@
+-- Task 4: Backup and Recovery
+
+-- Backup MySQL Database
+-- Run this in the terminal
+-- mysqldump -u root -p mydb > backup.sql
+
+-- Restore MySQL Database
+-- Run this in the terminal
+-- mysql -u root -p mydb < backup.sql
